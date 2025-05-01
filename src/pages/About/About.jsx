b202d6f75b7b39afd2e5e0481eda9bbc8f44b05a
Rafael Romano Silva <romanosilvarafael@gmail.com>
2025-05-01 11:20:36 -0300

    feat: adicionando a página about

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from './About.module.css'
+import { Link } from 'react-router-dom'
+
+const About = () => {
+  return (
+    <div className={styles.about}>
+        
+        <h2>
+            Sobre o Mini <span>Blog</span>
+        </h2>
+
+        <p>
+            Esse projeto consiste em um mini blog para desenvolvedores feito para uma avaliação!
+        </p>
+
+        <Link to="" className="btn">
+            Criar post
+        </Link>
+
+    </div>
+  )
+}
+
+export default About
