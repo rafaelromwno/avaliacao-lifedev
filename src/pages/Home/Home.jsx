80d45851b93448e5c665a57cf57d6fbfc19cd9c5
rafaelromwno <romanosilvarafael@gmail.com>
2025-05-07 15:19:05 -0300

    style: melhorando o estilo do botao "pesquisar"

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => {
               placeholder='Ou busque por tags...'
               onChange={(e) => setQuery(e.target.value)}
               />
-              <button className="btn btn-dark">Pesquisar</button>
+              <button className={styles.btnSearch}>Pesquisar</button>
           </form>
 
           {loading && <p>Carregando...</p>}
