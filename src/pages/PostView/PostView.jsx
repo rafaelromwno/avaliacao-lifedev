8cca86f993e697696e766180b6e00faa24de612b
Rafael Romano Silva <romanosilvarafael@gmail.com>
2025-05-09 14:57:27 -0300

    feat: adicionando botao de voltar para melhorar a usabilidade

--- src/pages/PostView/PostView.jsx
+++ src/pages/PostView/PostView.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './PostView.module.css'
 import { useParams } from 'react-router-dom'
 import { useFetchDocument } from '../../hooks/useFetchDocument'
+import { Link } from 'react-router-dom'
 
 const PostView = () => {
 
@@ -9,36 +10,45 @@ const PostView = () => {
     const { document: post } = useFetchDocument('posts', id)
 
   return (
-    <article className={styles.postContainer}>
-    {post && (
-        <>
-        <header className={styles.postHeader}>
-            <h1 className={styles.postTitle}>{post.title}</h1>
-            <figure className={styles.postImageWrapper}>
-            <img src={post.image} alt={post.title} className={styles.postImage} />
-            </figure>
-        </header>
+    <>
+        <article className={styles.postContainer}>
+        {post && (
+            <>
+            <header className={styles.postHeader}>
+                <h1 className={styles.postTitle}>{post.title}</h1>
+                <figure className={styles.postImageWrapper}>
+                <img src={post.image} alt={post.title} className={styles.postImage} />
+                </figure>
+            </header>
 
-        <section className={styles.postBody}>
-            <p>{post.content}</p>
-        </section>
-
-        {Array.isArray(post.tags) && post.tags.length > 0 && (
-            <section className={styles.postTagsSection}>
-            <h3 className={styles.tagsTitle}>Este post trata sobre:</h3>
-            <div className={styles.tagsWrapper}>
-                {post.tags.map((tag) => (
-                <span key={tag} className={styles.tag}>
-                    #{tag} 
-                </span>
-                ))}
-            </div>
+            <section className={styles.postBody}>
+                <p>{post.content}</p>
             </section>
+
+            {Array.isArray(post.tags) && post.tags.length > 0 && (
+                <section className={styles.postTagsSection}>
+                <h3 className={styles.tagsTitle}>Este post trata sobre:</h3>
+                <div className={styles.tagsWrapper}>
+                    {post.tags.map((tag) => (
+                    <span key={tag} className={styles.tag}>
+                        #{tag} 
+                    </span>
+                    ))}
+                </div>
+                </section>
+            )}
+
+            </>
         )}
-        </>
-    )}
-    </article>
-  )
+        </article>
+
+        <div style={{ textAlign: 'center' }}>
+            <Link to="/" className={`btn btn-dark ${styles.backButton}`}>
+                Voltar
+            </Link>
+        </div>
+    </>
+    )
 }
 
 export default PostView
